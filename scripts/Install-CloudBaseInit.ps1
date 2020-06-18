f0f949376edc45bd8a4c48dfbd01233a57ff294e
Ondřej Buday <331107@ucn.muni.cz>
2020-06-18 20:49:58 +0200

    Install cloud base init script

---
+++ scripts/Install-CloudBaseInit.ps1
@@ -0,0 +1,14 @@
+﻿# Check if C:\temp exists
+if(!(Test-Path -Path "C:\temp"))
+{
+    New-Item -Path c:\ -Name temp -ItemType Directory
+}
+
+# Download CloudbaseInitSetup_Stable_x64
+(New-Object System.Net.WebClient).DownloadFile('https://cloudbase.it/downloads/CloudbaseInitSetup_Stable_x64.msi','C:\temp\CloudbaseInitSetup_Stable_x64.msi')
+
+# Start Install CloudbaseInitSetup_Stable_x64
+
+Start-Process msiexec -ArgumentList '/i "C:\temp\CloudbaseInitSetup_Stable_x64.msi" /qb /norestart /l*v "c:\temp\install.log" USERNAME="vagrant" USERGROUPS="Administrators" INJECTMETADATAPASSWORD="TRUE" LOGGINGSERIALPORTNAME="COM1"' -Wait -PassThru
+
+Write-Host "Cloudbase-Init setup done!"
