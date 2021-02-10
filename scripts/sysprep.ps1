038fb0c77bd94d23d7371ac83626fd874cd31a93
Zdenek Vydra <xvydra@fi.muni.cz>
2021-02-10 22:22:44 +0100

    Update packer conf and sysprep for qemu and vbox

--- scripts/sysprep.ps1
+++ scripts/sysprep.ps1
@@ -1,5 +1,34 @@
+# Check if C:\temp exists
+if(!(Test-Path -Path "C:\temp"))
+{
+    New-Item -Path c:\ -Name temp -ItemType Directory
+}
 
-cd "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf"
+# Check if cloudbase-init is installed
+if (Test-Path -Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf") {
+    cd "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf"
+} else {
+    cd "C:\temp"
+    New-Item -Path c:\temp\ -Name Unattend.xml -ItemType File
+    Set-Content C:\temp\Unattend.xml '
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+  <settings pass="oobeSystem">
+    <component name="Microsoft-Windows-Shell-Setup" processorArchitecture="amd64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+      <OOBE>
+        <HideEULAPage>true</HideEULAPage>
+        <HideWirelessSetupInOOBE>true</HideWirelessSetupInOOBE>
+        <NetworkLocation>Work</NetworkLocation>
+        <ProtectYourPC>1</ProtectYourPC>
+        <SkipUserOOBE>true</SkipUserOOBE>
+        <SkipMachineOOBE>true</SkipMachineOOBE>
+      </OOBE>
+    </component>
+  </settings>
+</unattend>
+'
+}
+
 Start-Process -FilePath "C:\Windows\System32\sysprep\sysprep.exe" -ArgumentList '/generalize /oobe /unattend:Unattend.xml /quit'
 
 # wait for sysprep to finish
