4509d67670225899f3b866550511d1d9e18aabb9
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-18 10:19:11 +0100

    Update cloudbase-init configuration

--- scripts/Install-CloudBaseInit.ps1
+++ scripts/Install-CloudBaseInit.ps1
@@ -14,15 +14,41 @@ Start-Process msiexec -ArgumentList '/i "C:\temp\CloudbaseInitSetup_Stable_x64.m
 # Don't require changing password on first logon, disable winrm basic auth and http listener
 Add-Content "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\conf\cloudbase-init.conf" '
 first_logon_behaviour=no
-winrm_configure_http_listener=False
-winrm_enable_basic_auth=False
-plugins=cloudbaseinit.plugins.common.mtu.MTUPlugin, cloudbaseinit.plugins.windows.ntpclient.NTPClientPlugin, cloudbaseinit.plugins.common.sethostname.SetHostNamePlugin, cloudbaseinit.plugins.windows.createuser.CreateUserPlugin, cloudbaseinit.plugins.common.networkconfig.NetworkConfigPlugin, cloudbaseinit.plugins.common.setuserpassword.SetUserPasswordPlugin, cloudbaseinit.plugins.windows.winrmlistener.ConfigWinRMListenerPlugin, cloudbaseinit.plugins.windows.winrmcertificateauth.ConfigWinRMCertificateAuthPlugin, cloudbaseinit.plugins.common.sshpublickeys.SetUserSSHPublicKeysPlugin, cloudbaseinit.plugins.windows.extendvolumes.ExtendVolumesPlugin, cloudbaseinit.plugins.common.userdata.UserDataPlugin, cloudbaseinit.plugins.common.localscripts.LocalScriptsPlugin, cloudbaseinit.plugins.windows.licensing.WindowsLicensingPlugin
+activate_windows=true
+plugins=cloudbaseinit.plugins.common.mtu.MTUPlugin, cloudbaseinit.plugins.windows.ntpclient.NTPClientPlugin, cloudbaseinit.plugins.windows.createuser.CreateUserPlugin, cloudbaseinit.plugins.common.networkconfig.NetworkConfigPlugin, cloudbaseinit.plugins.common.setuserpassword.SetUserPasswordPlugin, cloudbaseinit.plugins.common.localscripts.LocalScriptsPlugin, cloudbaseinit.plugins.windows.winrmcertificateauth.ConfigWinRMCertificateAuthPlugin, cloudbaseinit.plugins.common.sshpublickeys.SetUserSSHPublicKeysPlugin, cloudbaseinit.plugins.windows.extendvolumes.ExtendVolumesPlugin, cloudbaseinit.plugins.common.userdata.UserDataPlugin, cloudbaseinit.plugins.windows.licensing.WindowsLicensingPlugin
+metadata_services=cloudbaseinit.metadata.services.configdrive.ConfigDriveService, cloudbaseinit.metadata.services.httpservice.HttpService
 '
 
 # Set registry key so SetUserPasswordPlugin doesn't run on reboot even if it fails
 New-Item -Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts" -Name "SetUserPasswordPlugin-fix.ps1" -ItemType "file" -Value '
 $p = reg query "HKLM\SOFTWARE\Cloudbase Solutions\Cloudbase-Init"
 reg add "$p\Plugins" /v SetUserPasswordPlugin /t REG_DWORD /d 1 /f
+'
+
+# Wait for WinRM to start to be able to set WinRM auth certificate
+New-Item -Path "C:\Program Files\Cloudbase Solutions\Cloudbase-Init\LocalScripts" -Name "WaitForWinRM.ps1" -ItemType "file" -Value '
+$timeout = 300
+$retryInterval = 5
+$startTime = Get-Date
+while ((Get-Date) -lt $startTime.AddSeconds($timeout)) {
+    try {
+        $winRMTest = Test-WSMan -ErrorAction Stop
+
+        if ($winRMTest) {
+            Write-Host "WinRM is available."
+            break
+        }
+    }
+    catch {
+        Write-Host "WinRM is not available yet. Retrying in $retryInterval seconds..."
+    }
+
+    Start-Sleep -Seconds $retryInterval
+}
+
+if ((Get-Date) -ge $startTime.AddSeconds($timeout)) {
+    Write-Host "Timeout: WinRM is still not available after $timeout seconds."
+}
 '
 
 Write-Host "Cloudbase-Init setup done!"
