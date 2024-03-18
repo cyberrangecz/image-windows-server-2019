449f9021c4634c7099002334ba0341e6dd68dcd2
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-18 10:23:07 +0100

    Update shrinking of disk

--- scripts/shrink-filesystem.ps1
+++ scripts/shrink-filesystem.ps1
@@ -1,6 +1,6 @@
 # Get information about the system drive
 $systemDrive = Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -eq 'C' }
-
+$partition = Get-Partition -DriveLetter 'C'
 # Get the used space on the system drive
 $usedSpaceGB = [math]::Round($systemDrive.Used / 1GB, 2)
 
@@ -11,4 +11,8 @@ $updatedSizeGB = $usedSpaceGB + 2
 $updatedSizeBytes = $updatedSizeGB * 1GB
 
 # Resize the system partition with the updated size
-Resize-Partition -DriveLetter 'C' -Size $updatedSizeBytes
+@"
+sel disk $($partition.DiskNumber)
+sel part $($partition.PartitionNumber)
+shrink
+"@ | diskpart
