73077d97eed73f91f0b2ac7ca58805b5e0b94843
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-19 13:59:27 +0100

    Defragment disk before shrink

--- scripts/shrink-filesystem.ps1
+++ scripts/shrink-filesystem.ps1
@@ -1,14 +1,8 @@
 # Get information about the system drive
-$systemDrive = Get-PSDrive -PSProvider FileSystem | Where-Object { $_.Name -eq 'C' }
 $partition = Get-Partition -DriveLetter 'C'
-# Get the used space on the system drive
-$usedSpaceGB = [math]::Round($systemDrive.Used / 1GB, 2)
 
-# Add 2 GB to the used space
-$updatedSizeGB = $usedSpaceGB + 2
-
-# Convert Variable
-$updatedSizeBytes = $updatedSizeGB * 1GB
+# Defrag disk
+Optimize-Volume -DriveLetter 'C' -Defrag -Retrim
 
 # Resize the system partition with the updated size
 @"
