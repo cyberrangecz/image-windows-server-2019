f71c66fe4c35d73a7ccfde3ec169e4e457d59316
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-20 12:42:16 +0100

    Fix certutil

--- scripts/spiceTools.ps1
+++ scripts/spiceTools.ps1
@@ -1,5 +1,5 @@
 #Write-Verbose "SPICE TOOLS" "Installing..."
 #Write-Verbose "SPICE TOOLS" "Importing Red Hat Certificate..."
-certutil -AddStore "TrustedPublisher" "a:\redhat.cer"
+certutil -f -AddStore "TrustedPublisher" "a:\redhat.cer"
 Start-Process -FilePath "C:\Windows\Temp\spice-guest-tools.exe" -ArgumentList "/S" -Wait
 #Write-Verbose "SPICE TOOLS" "Successfully Installed."
