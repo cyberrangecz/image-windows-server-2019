ae93f567a4450388705dcc3908df3ca58fbd7262
Zdeněk Vydra <485514@mail.muni.cz>
2024-03-20 19:59:39 +0100

    Split defrag into two

--- scripts/shrink-filesystem.ps1
+++ scripts/shrink-filesystem.ps1
@@ -2,7 +2,8 @@
 $partition = Get-Partition -DriveLetter 'C'
 
 # Defrag disk
-defrag C: /L /D /K /G /X /H
+defrag C: /L /D /K /G /H
+defrag C: /X /H
 
 # Resize the system partition with the updated size
 @"
